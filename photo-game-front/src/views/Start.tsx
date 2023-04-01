c631427fb32354ac147d252477837887ff475bb9
Weronika Witek <weronikaj.witek@gmail.com>
2023-04-01 18:02:08 +0200

    something

--- photo-game-front/src/views/Start.tsx
+++ photo-game-front/src/views/Start.tsx
@@ -1,14 +1,26 @@
 import {Outlet, useNavigate} from "react-router-dom";
 import {Button, FormControl, FormLabel, Grid, IconButton, TextField} from "@mui/material";
-import {useState} from "react";
-import {fetchApi} from "../utils/fetchApi";
+import {createContext, useMemo, useState} from "react";
+import {addAuthToken, fetchApi} from "../utils/fetchApi";
 import {useNickname} from '../contexts/NicknameContext'
 import SettingsIcon from '@mui/icons-material/Settings';
+
+const settingsContext = createContext('')
 
 function Start() {
     const navigate = useNavigate();
 
     const [nick, setNick] = useState('');
+    const [roundNumber, setRoundNumber] = useState('');
+    const state = useMemo(() => ({
+        roundNumber,
+        setRoundNumber: (roundNumber) => {
+            setRoundNumber(roundNumber);
+            localStorage.setItem('roundNumber', roundNumber);
+            addAuthToken(roundNumber);
+        }
+    }), [roundNumber]);
+    const [timeLimit, setTimeLimit] = useState('');
     const {setNickname} = useNickname();
 
     const handleClick = async (event: { preventDefault: () => void; }) => {
@@ -27,33 +39,34 @@ function Start() {
 
     return (
         <Grid container justifyContent={"center"}>
-            <FormControl>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <FormLabel>Enter your nickname:</FormLabel>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <IconButton onClick={() => navigate('settings')}>
-                            <SettingsIcon/>
-                        </IconButton>
-                    </Grid>
+            <Grid container>
+                <Grid item xs={6} >
+                    <FormLabel>Enter your nickname:</FormLabel>
                 </Grid>
-                <Grid item xs={12}>
-                    <TextField onChange={(e) => {
-                        setNick(e.target.value)
-                    }} value={nick} variant={"outlined"}></TextField>
+                <Grid item xs={6} display={"flex"} justifyContent={"flex-end"}>
+                    <IconButton onClick={() => navigate('settings')}>
+                        <SettingsIcon/>
+                    </IconButton>
                 </Grid>
-                <Grid item xs={12} alignSelf={"stretch"}>
-                    <Button
-                        onClick={(event) => {
-                            handleClick(event)
-                        }}
-                        variant="contained"
-                        fullWidth
-                    >Start the game</Button>
-                </Grid>
-            </FormControl>
-            <Outlet/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField onChange={(e) => {
+                    setNick(e.target.value)
+                }} value={nick} variant={"outlined"}
+                fullWidth={true}></TextField>
+            </Grid>
+            <Grid item xs={12} alignSelf={"stretch"}>
+                <Button
+                    onClick={(event) => {
+                        handleClick(event)
+                    }}
+                    variant="contained"
+                    fullWidth={true}
+                >Start the game</Button>
+            </Grid>
+        {/*<settingsContext.Provider value={state}>*/}
+        {/*    <Outlet/>*/}
+        {/*</settingsContext.Provider>*/}
         </Grid>
     )
 }
