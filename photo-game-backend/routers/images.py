751150f0965a6711ed105d41a154fa7c11e8f164
StormyData <30272275+StormyData@users.noreply.github.com>
2023-04-01 11:34:52 +0200

    mocked 2 images, mock image ids: im-advjlgjlesa and im-ajsofeaokae

--- photo-game-backend/routers/images.py
+++ photo-game-backend/routers/images.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse
 
 router = APIRouter(
-    prefix="/image/"
+    prefix="/image",
+    tags=["images"]
 )
 
+mock_image_dictionary = {
+    "im-advjlgjlesa": "mock-images/512x512-png-images-3-3437135354.png",
+    "im-ajsofeaokae": "mock-images/BMP-1816717683.png"
+}
 
-@router.get("/{imageid}")
-def get_image(imageid: int):
-    return {"sth": "std"}
+@router.get("/{imageid}", response_class=FileResponse)
+async def get_image(imageid: str):
+    if imageid not in mock_image_dictionary:
+        raise HTTPException(status_code=404)
+
+    return FileResponse(mock_image_dictionary[imageid], media_type="image/png")
