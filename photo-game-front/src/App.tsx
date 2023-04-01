a0387dcc7d4070a8637a057b0ac94152716d9bb9
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2023-04-01 10:35:48 +0200

    build fix

--- photo-game-front/src/App.tsx
+++ photo-game-front/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Box, Container, useTheme } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
--- photo-game-front/src/index.tsx
+++ photo-game-front/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const router = createBrowserRouter([
   {
