a469f1a422df92104f2b976ae2ed3e93b820434b
Mapet <jakubsordyl1@gmail.com>
2023-04-01 11:25:38 +0200

    mock game api

--- photo-game-backend/routers/game.py
+++ photo-game-backend/routers/game.py
@@ -4,3 +4,26 @@ router = APIRouter(
     prefix="/game/"
 )
 
+@router.post("/")
+def create_game():
+    pass
+
+@router.get("/{game_id}")
+def get_all_game_data(game_id: int):
+    pass
+
+@router.get("/{game_id}/{round_id}")
+def get_round_all_data():
+    pass
+
+@router.get("/{game_id}/{round_id}/prompts")
+def get_rounds_prompts():
+    pass
+
+@router.get("/{game_id}/{round_id}/images")
+def get_rounds_images():
+    pass
+
+@router.post("/{game_id}/{round_id}/match")
+def post_match():
+    pass
