d05a34192d0033b97b65446e2460703c31191247
Weronika Witek <weronikaj.witek@gmail.com>
2023-04-01 14:25:07 +0200

    style change

--- photo-game-front/src/views/Start.tsx
+++ photo-game-front/src/views/Start.tsx
@@ -1,7 +1,8 @@
 import {useNavigate} from "react-router-dom";
-import {Button, FormControl, FormLabel, Grid, TextField} from "@mui/material";
+import {Button, FormControl, FormLabel, Grid, IconButton, TextField} from "@mui/material";
 import {useState} from "react";
 import {fetchApi} from "../utils/fetchApi";
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Start() {
     const navigate = useNavigate();
@@ -12,16 +13,24 @@ function Start() {
     const handleClick = (event: { preventDefault: () => void; })  => {
         event.preventDefault();
 
+        // TODO set nickname using context
+
+        // get game id
         fetchApi('/').then(response => setGameName(response));
 
-        navigate('/game');
+        navigate("/game/" + gameName);
       };
 
     return (
-        <Grid container>
+        <Grid container justifyContent={"center"}>
             <FormControl>
-                <Grid item xs={12}>
-                    <FormLabel>Enter your nickname:</FormLabel>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <FormLabel>Enter your nickname:</FormLabel>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <IconButton>SettingsIcon</IconButton>
+                    </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     <TextField onChange={(e) => {setNick(e.target.value)}} value={nick} variant={"outlined"}></TextField>
