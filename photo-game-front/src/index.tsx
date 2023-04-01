7206d27d7a343f223b14355bdb4ab95ebf2a7f77
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2023-04-01 10:26:42 +0200

    front init

--- photo-game-front/src/index.tsx
+++ photo-game-front/src/index.tsx
@@ -3,14 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "start",
+        element: <div>Start</div>,
+      },
+      {
+        path: 'game',
+        element: <div>Game</div>,
+      }
+    ],
+  },
+]);
+
+const theme = createTheme({
+
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
