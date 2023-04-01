412f9a28c30a547d890ef810dc761ebe96217c6d
K3;3 <41787377+Wisien999@users.noreply.github.com>
2023-04-01 11:53:07 +0200

    state machine

--- photo-game-backend/game_state_machine.py
+++ photo-game-backend/game_state_machine.py
@@ -1,23 +1,28 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import HTTPException
 
 
 class GameState:
     def __init__(self):
-        self.rounds: dict[int, Round] = {0: Round()}
+        self.rounds: dict[str, Round] = {'0': Round()}
 
 
 class Round:
-    def __init__(self):
+    def __init__(self, solution: dict[str, str] = None):
+        self.solution: dict[str, str] = solution or dict()
         self.all_prompts: set[int] = set()
         self.all_images: set[int] = set()
         self.image_to_prompt: dict[int, int] = dict()
 
-    def points(self):
-        if len(self.all_images) ==
+    def points(self) -> int:
+        if len(self.all_images) != len(self.all_prompts):
+            raise HTTPException(404)
+
+        return  len(self.all_prompts)
 
 
-games: dict[int, GameState] = dict()
+
+games: dict[str, GameState] = dict()
 
 
-def get_points()
+def get_points(game_id: str, round_id: str) -> int:
+    return games[game_id].rounds[round_id].points()
