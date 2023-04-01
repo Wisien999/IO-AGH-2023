2553d9da477b6e1515236cfc6305c3a4f30edab7
K3;3 <41787377+Wisien999@users.noreply.github.com>
2023-04-01 11:53:07 +0200

    state machine

--- photo-game-backend/routers/game.py
+++ photo-game-backend/routers/game.py
@@ -30,14 +30,17 @@ def get_rounds_images():
 class UserAction(BaseModel):
     actions: dict[str, str]  # prompt id -> image id
 
+class MatchResult(BaseModel):
+
+
 
 @router.post("/{game_id}/{round_id}/match")
-async def user_action(game_id: int, round_id: int, user_action: UserAction):
+async def user_action(game_id: str, round_id: str, user_action: UserAction):
     for prompt, image_id in user_action.actions:
         del games[game_id].rounds[round_id].image_to_prompt[image_id]
-        del games[game_id].rounds[round_id].image_to_prompt[prompt]
+        games[game_id].rounds[round_id].image_to_prompt = {k: v for k, v in games[game_id].rounds[round_id].image_to_prompt.items() if v != prompt}
 
     for prompt, image_id in user_action.actions:
         games[game_id].rounds[round_id].image_to_prompt[image_id] = prompt
 
-    pass
+    return
